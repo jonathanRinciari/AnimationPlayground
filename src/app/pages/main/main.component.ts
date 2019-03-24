a25b6fefdd2546d68209f95618b7224319feb988
Jonathan Rinciari <jrinciaritosd@gmail.com>
2019-03-24 19:44:11 -0400

    Added additionally styling to main window

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,18 +4,28 @@ const MenuButtonAnimation = trigger('menuState', [
   state('hide', style({ transform: 'rotate(0)' })),
   state('show', style({ transform: 'rotate(-180deg)' })),
   transition('hide => show', [style({transform: 'rotate(-360deg)'}), animate('350ms ease-out')]),
-  transition('show => hide', animate('350ms ease-in'))
+  transition('show => hide', animate('350ms ease-in '))
 ]);
 
-// const Keyframe = trigger('menuState', [
-//   transition('* => move',
-//   animate('2000ms', keyframes([
-//     style({transform: 'translateX(0)    rotateY(0)',        offset: 0}),
-//     style({transform: 'translateX(50%)  rotateY(90deg)',    offset: 0.33}),
-//     style({transform: 'translateY(-75%) rotateY(180deg)',   offset: 0.66}),
-//     style({transform: 'translateX(-100%)',                  offset: 1.0})
-//   ]))
-// )]);
+const spinner = trigger('spinner', [
+  transition('* <=> *', [style({transform: 'rotate(-360deg)'}), animate('800ms ease-out')]),
+]);
+
+const spinnerRight = trigger('spin', [
+  state('hide', style({ transform: 'rotate(0deg)' })),
+  state('show', style({ transform: 'rotate(0deg)' })),
+  transition('* <=> *', [style({transform: 'rotate(360deg)'}), animate('800ms ease-out')]),
+]);
+
+const Keyframe = trigger('menuState', [
+  transition('* => move',
+  animate('2000ms', keyframes([
+    style({transform: 'translateX(0)    rotateY(0)',        offset: 0}),
+    style({transform: 'translateX(50%)  rotateY(90deg)',    offset: 0.33}),
+    style({transform: 'translateY(-75%) rotateY(180deg)',   offset: 0.66}),
+    style({transform: 'translateX(-100%)',                  offset: 1.0})
+  ]))
+)]);
 
 
 const MultiState = trigger('menuState', [
@@ -35,10 +45,12 @@ const MultiState = trigger('menuState', [
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  animations: [MultiState]
+  animations: [spinner, spinnerRight]
 })
 export class MainComponent implements OnInit {
   state = false;
+  counter = 0;
+  times = 5;
 
   constructor() { }
 
@@ -52,6 +64,16 @@ export class MainComponent implements OnInit {
   toggle() {
     console.log(this.state);
     this.state = !this.state;
+  }
+
+
+  onDone($event) {
+    // call this function at the end of the previous animation.
+    // run it as many time as defined
+    if (this.counter < this.times) {
+      this.toggle();
+      this.counter++;
+    }
   }
 
 
